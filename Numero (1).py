6231a6817f224cf6086df3ece2d24f6df755e92d
Sampreeth47 <sampreeth4127@gmail.com>
2025-11-19 12:26:12 +0530

    Add files via upload

---
+++ Numero (1).py
@@ -0,0 +1,14 @@
+print('\t\t\t\033[94mNUMERA\033[0m')
+n=input('\033[97mName : ')
+print(f'ನಮಸ್ಕಾರ {n}!! Welcome to NUMERA.....')
+print("\033[91mಎಚ್ಚರಿಕೆ: ಬುದ್ಧಿವಂತರಿಗೆ ಮಾತ್ರ!!")
+
+print('\033[97mList of Topics : \n1. Algebra \n2. Limits & Derivatives \n3. Integrals \n4. Vector algebra \n5. Permutation and combination \n6. Complex numbers \n7. Trigonometry')
+print('\n7 Topics!! \033[93m#ThalaForAReason')
+
+print('\033[97m')
+c=int(input('Choose a topic(number)'))
+
+def Algebra():
+    print ('\033[97mSelect the level: \n1. \033[92mEasy \033[97m\n2. \033[38;5;208mMedium \033[97m\n3. \033[91mHard')
+    
